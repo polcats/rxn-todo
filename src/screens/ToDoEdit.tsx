e4c7c8953dc95de8a43f39f10ec3b00d1260fc51
Paul Catalan <24451518+polcats@users.noreply.github.com>
2020-07-23 09:47:49 +0800

    Imported useContext.

--- src/screens/ToDoEdit.tsx
+++ src/screens/ToDoEdit.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { observer } from 'mobx-react-lite';
 import { StyleSheet, Text, View } from 'react-native';
 import { AppContext } from '../models/index';
