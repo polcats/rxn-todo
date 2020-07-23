9a9d3832ea260d2c78b9a5ad34bfe3d6c2932b61
Paul Catalan <24451518+polcats@users.noreply.github.com>
2020-07-23 09:46:27 +0800

    Added import for AppContext.

--- src/screens/ToDoList.tsx
+++ src/screens/ToDoList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppContext } from '../models/index';
 
 const ToDoList: React.FC = () => {
   return <Text>To do list...</Text>;
